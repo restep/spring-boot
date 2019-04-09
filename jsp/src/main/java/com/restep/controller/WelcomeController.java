0ba6050ae09c829893ef92d127bb1c5bcec16140
restep <restep@163.com>
2019-04-09 17:49:36 +0800

    springboot jsp

---
+++ jsp/src/main/java/com/restep/controller/WelcomeController.java
@@ -0,0 +1,36 @@
+package com.restep.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author restep
+ * @date 2019/4/9
+ */
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", "hello world");
+
+        return "welcome";
+    }
+
+    @GetMapping("/user")
+    public String user(Map<String, Object> model, HttpServletRequest request) {
+        model.put("username", "neo");
+        model.put("salary", 666);
+        request.getSession().setAttribute("count", 6);
+        return "user";
+    }
+}
