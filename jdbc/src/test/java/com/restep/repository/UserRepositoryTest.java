55aaf1eb6e8aab8b2d35714c34a9290ad1c32b78
restep <restep@163.com>
2019-04-10 18:29:16 +0800

    jdbc

---
+++ jdbc/src/test/java/com/restep/repository/UserRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.restep.repository;
+
+import com.restep.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @author restep
+ * @date 2019/4/10
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSave() {
+        User user = new User("neo", "123456", 30);
+        userRepository.save(user);
+    }
+
+    @Test
+    public void testUpdate() {
+        User user = new User("neo", "123456", 18);
+        user.setId(1L);
+        userRepository.update(user);
+    }
+
+    @Test
+    public void testDetele() {
+        userRepository.delete(1L);
+    }
+
+    @Test
+    public void testQueryOne() {
+        User user = userRepository.findById(1L);
+        System.out.println("user == " + user.toString());
+    }
+
+    @Test
+    public void testQueryAll() {
+        List<User> users = userRepository.findALL();
+        for (User user : users) {
+            System.out.println("user == " + user.toString());
+        }
+    }
+}
