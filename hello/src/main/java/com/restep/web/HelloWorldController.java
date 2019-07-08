beea4f02a65c6ed2e4fbe45e00b654d7187673fb
restep <restep@163.com>
2019-07-08 17:08:46 +0800

    第二遍学习开始

--- hello/src/main/java/com/restep/web/HelloWorldController.java
+++ hello/src/main/java/com/restep/web/HelloWorldController.java
@@ -1,17 +1,22 @@
 package com.restep.web;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author restep
  * @date 2019/4/9
- * @RestController 的意思是 Controller 里面的方法都以 JSON 格式输出 不需要有其他额外的配置
+ * @RestController 的意思是 Controller 里面的方法都以JSON格式输出 不需要有其他额外的配置
  */
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
-    public String index(String name) {
+    public String hello(String name) {
+        if (StringUtils.isEmpty(name)) {
+            name = "";
+        }
+
         return "Hello World " + name;
     }
 }
--- hello/src/test/java/com/restep/web/HelloTest.java
+++ hello/src/test/java/com/restep/web/HelloTest.java
@@ -1,5 +1,6 @@
 package com.restep.web;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
@@ -22,11 +24,39 @@ public class HelloTest {
         mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
     }
 
+    /**
+     * 单元测试
+     */
     @Test
-    public void getHello() throws Exception {
+    public void hello() {
+        System.out.println("hello world");
+    }
+
+    /**
+     * post请求 返回全部信息
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ajaxHello() throws Exception {
         //返回Body = hello world 小明 则请求成功
-        mockMvc.perform(MockMvcRequestBuilders.post("/hello?name=小明").accept(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.post("/hello?name=小明")
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+        .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    /**
+     * post请求
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ajaxHelloFilter() throws Exception {
+        //根据关键字过滤掉次要的信息
+        mockMvc.perform(MockMvcRequestBuilders.post("/hello?name=小明")
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("小明")));
 
     }
 }
