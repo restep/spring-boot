2d3aed03340be8b45b3dbc5a6b6a3346183d4456
restep <restep@163.com>
2020-04-13 16:23:45 +0800

    String+

---
+++ src/test/java/com/restep/collections/StringTest.java
@@ -0,0 +1,11 @@
+package com.restep.collections;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String str1 = "a";
+        String str2 = "b";
+        String str3 = str1 + str2;
+        //第二种方式可以使用加号
+        //String str = "a" + "b" + "c";
+    }
+}
